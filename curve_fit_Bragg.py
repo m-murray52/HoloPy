67a9591b39e3d9eb5b32608bf2c6006c8fcc83d8
Michael Murray <m.murray52@nuigalway.ie>
2022-06-08 14:24:35 +0100

    Caluclate std dev

--- curve_fit_Bragg.py
+++ curve_fit_Bragg.py
@@ -44,10 +44,8 @@ class Grating:
 
 def main():
     
+    # Load experimental data
     file = args.file
-#file ='/home/michael/Documents/PhD_docs/16Feb22_VerdiLab/Bragg_CSV/MM16Feb_S5_R1_bgB.csv'
-#file = '/home/michael/Documents/PhD_docs/23Feb_Verdi/Bragg_CSV/S3A_bragg_03_100.csv'
-# Load experimental data
     df = pd.read_csv(file)
     angles = df.Angle
     diff_efficiencies = list((np.max(df.DE) - df.DE)*0.01)
@@ -68,59 +66,57 @@ def main():
     thickness = grating1.film_thickness
 
     def diffraction_efficiency(bragg_deviation, RIM, thickness):
-        # Convert angles to rads
-    #bragg_deviation = np.deg2rad(bragg_deviation)
-    # find deviation in film
         bragg_deviation = np.arcsin((np.sin(np.deg2rad(bragg_deviation)))/n_film)
         E = (bragg_deviation*2*np.pi*n_film*thickness*np.sin(bragg_angle))/(wavelength_air)
         v = (np.pi*RIM*thickness)/(wavelength_air*np.cos(bragg_angle))
+        return (np.sin(np.sqrt((v)**2 + E**2)))**2/(1+(E**2/(v)**2))
 
-        #de= (np.sin(np.sqrt((np.pi/2)**2 + off_braggs**2))**2)/(1+(off_braggs**2/(np.pi/2)**2))
-        #return np.sin(v**2 + E**2)
-        de= np.sin(np.sqrt((v)**2 + E**2))**2/(1+(E**2/(v)**2))
-        return de
+        
 
     def theoretical_diffraction_efficiency(bragg_deviation, v):
-        # Convert angles to rads
-        # Convert to angle in film
         bragg_deviation = np.arcsin((np.sin(np.deg2rad(bragg_deviation)))/n_film)
         E = (bragg_deviation*2*np.pi*n_film*thickness*np.sin(bragg_angle))/(wavelength_air)
-        #v = (np.pi*RIM*thickness)/(wavelength_air*np.cos(bragg_angle))
-        #de= (np.sin(np.sqrt((np.pi/2)**2 + off_braggs**2))**2)/(1+(off_braggs**2/(np.pi/2)**2))
-        #return np.sin(v**2 + E**2)
-        de= np.sin(np.sqrt((v)**2 + E**2))**2/(1+(E**2/(v)**2))
-        return de
+        return (np.sin(np.sqrt((v)**2 + E**2)))**2/(1+(E**2/(v)**2))
+       
 
     plt.scatter(angles, diff_efficiencies, s= 20, label='measured data')
+
+    
     popt, pcov = curve_fit(diffraction_efficiency, angles, diff_efficiencies, bounds=(0.000, [0.006, 55]))
     popt
 
+    perr = np.sqrt(np.diag(pcov))
+    perr_RIM = float(perr[0])
+    perr_T = float(perr[1])
 
-    v = (np.pi*popt*thickness)/(wavelength_air*np.cos(bragg_angle))  
+    # phase parameter determined by curve fitting
     RIM = float(popt[0])
-    bestfit_thickness = float(popt[1])
+    curve_fit_thickness = float(popt[1])
+    v = (np.pi*popt*curve_fit_thickness)/(wavelength_air*np.cos(bragg_angle))  
+    
 
-    plt.plot(angles, diffraction_efficiency(angles, *popt), 'r-', label='Best-fit, RIM = {0:.3g},'.format(RIM) + ' T = {0:.3g}'.format(bestfit_thickness) + r'$\mu m$')
+    
     # , RIM = {0:.3g}'.format(RIM)
     DE_max = np.max(diff_efficiencies)
 
     # Analytically derived phase parameter
     v = np.arcsin(np.sqrt(DE_max))
-    #v = np.pi/2
     RIM = float((v*wavelength_air*np.cos(bragg_angle))/(np.pi*thickness))
     DE = [theoretical_diffraction_efficiency(angle, v) for angle in angles]
     print(len(angles))
     print(len(DE))
 
 
+    #plt.plot(angles, diffraction_efficiency(angles, *popt), 'r-', label='Best-fit, RIM = {0:.3g},'.format(RIM) + ' T = {0:.3g}'.format(curve_fit_thickness)  + r'$\mu m$')
+    plt.plot(angles, diffraction_efficiency(angles, *popt), 'r-', label='Best-fit, RIM = {0:.3g},'.format(RIM) + '+/- {0:.3g}'.format(perr_RIM) + ' T = {0:.3g}'.format(curve_fit_thickness) + '+/- {0:.3g}'.format(perr_T) + r'$\mu m$')
     # Plot DE vs delta_theta
     plt.plot(angles, DE, 'g-', label='Analytical, RIM = {0:.3g},'.format(RIM) + ' T = {0:.3g}'.format(thickness) + r'$\mu m$')
  
-#angles = np.arange(0,len(angles),1)
-#DE = np.arange(0,len(DE),1)
+    #angles = np.arange(0,len(angles),1)
+    #DE = np.arange(0,len(DE),1)
     spline = UnivariateSpline(angles, DE-np.max(DE)/2, s=0)
     r1, r2 = spline.roots() # find the roots
-    plt.hlines(np.max(DE)/2, r1, r2, alpha = 0.75)
+    #plt.hlines(np.max(DE)/2, r1, r2, alpha = 0.75)
 
     plt.title(r'DE vs $\Delta \theta$, Thickness $= 50 \mu m$')
     plt.xlabel(r'$\Delta \theta_{air} (\circ)$')
@@ -140,23 +136,11 @@ def main():
     print('RIM', RIM)
     print('Delta theta', grating1.spatial_frequency*thickness)
 
-
+    #print(perr_RIM)
+    print(np.shape(pcov))
+    print(pcov)
+    print(perr)
 
 if __name__ == '__main__':
     print(__doc__)
     main()
-
-"""# Define recording beam angles
-beam1_angle = 30
-beam2_angle = -30
-
-# Correct for material refractive index, for unslanted grating this is equivalent to the Bragg angle at the recording wavelength
-# Angles in rads
-beam1_angle_medium = np.arcsin((np.sin(np.deg2rad(beam1_angle)))/grating1.n_film) 
-beam2_angle_medium = np.arcsin((np.sin(np.deg2rad(beam2_angle)))/grating1.n_film) 
-
-# range of bragg deviations in rads
-bragg_deviation = np.linspace(-beam1_angle_medium/2, beam1_angle_medium/2, 10000)
-
-# range of bragg deviations in air (degress)
-bragg_deviation_in_air = np.linspace(-beam1_angle/2, beam1_angle/2, 10000)"""
